f3c00e0c52e56342da8b8126a36a873c1136cb9c
bikeshrai78 <bikeshrai78@gmail.com>
2022-02-23 12:05:29 +0545

    Add createTaskCard()

---
+++ src/createTaskCard.js
@@ -0,0 +1,50 @@
+export default function createTaskCard(){
+    let sectionTask = document.querySelector('.section-tasks');
+
+let checkbox = document.createElement('input');
+let taskItem = document.createElement('p');
+let context = document.createElement('p');
+let priority = document.createElement('p');
+let dueDate = document.createElement('p');
+let edit = document.createElement('p');
+let deleteTask = document.createElement('p');      
+
+let checkboxContainer = document.createElement('div');
+let taskTest = document.createElement('div');
+let contextTag = document.createElement('div');
+let priorityTag = document.createElement('div');
+let dueTag = document.createElement('div');
+let editOption = document.createElement('div');
+let deleteContainer = document.createElement('div');
+
+checkboxContainer.classList.add('checkbox-container');
+taskTest.classList.add('task-text');
+contextTag.classList.add('context-tag');
+priorityTag.classList.add('priority-tag');
+dueTag.classList.add('due-tag');
+editOption.classList.add('edit-option');
+deleteContainer.classList.add('delete-container');
+
+checkboxContainer.appendChild(checkbox);
+taskTest.appendChild(taskItem);
+contextTag.appendChild(context);
+priorityTag.appendChild(priority);
+dueTag.appendChild(dueDate);
+editOption.appendChild(edit);
+deleteContainer.appendChild(deleteTask);
+
+let taskContainer = document.createElement('div');
+taskContainer.classList.add('task-container');
+taskContainer.appendChild(checkboxContainer);
+taskContainer.appendChild(taskTest);
+taskContainer.appendChild(contextTag);
+taskContainer.appendChild(priorityTag);
+taskContainer.appendChild(dueTag);
+taskContainer.appendChild(editOption);
+taskContainer.appendChild(deleteContainer);
+
+checkbox.type = 'checkbox';
+edit.textContent = 'Edit';
+deleteTask.textContent = 'X';
+sectionTask.appendChild(taskContainer.cloneNode(true));
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-
+import createTaskCard from './createTaskCard';
 /* const getTaskItem = (data) => {
     let taskItem = data.taskItem;
     return {taskItem};
@@ -46,59 +46,10 @@ function getProjectName(task){
     return task.projectName
 }
 
+createTaskCard();
 
-let checkbox = document.createElement('input');
-let sectionTask = document.querySelector('.section-tasks');
-let taskItem = document.createElement('p');
-let context = document.createElement('p');
-let priority = document.createElement('p');
-let dueDate = document.createElement('p');
-let edit = document.createElement('p');
-let deleteTask = document.createElement('p');
-
-checkbox.type = 'checkbox';
-console.log(checkbox);
-taskItem.textContent = getTaskItem(task1);
-context.textContent = getContext(task1);
-priority.textContent = getPriority(task1);
-dueDate.textContent = getDueDate(task1);
-edit.textContent = 'Edit';
-deleteTask.textContent = 'X';
-
-let checkboxContainer = document.createElement('div');
-let taskTest = document.createElement('div');
-let contextTag = document.createElement('div');
-let priorityTag = document.createElement('div');
-let dueTag = document.createElement('div');
-let editOption = document.createElement('div');
-let deleteContainer = document.createElement('div');
-
-checkboxContainer.classList.add('checkbox-container');
-taskTest.classList.add('task-text');
-contextTag.classList.add('context-tag');
-priorityTag.classList.add('priority-tag');
-dueTag.classList.add('due-tag');
-editOption.classList.add('edit-option');
-deleteContainer.classList.add('delete-container');
-
-checkboxContainer.appendChild(checkbox);
-taskTest.appendChild(taskItem);
-contextTag.appendChild(context);
-priorityTag.appendChild(priority);
-dueTag.appendChild(dueDate);
-editOption.appendChild(edit);
-deleteContainer.appendChild(deleteTask);
-
-let taskContainer = document.createElement('div');
-taskContainer.classList.add('task-container');
-taskContainer.appendChild(checkboxContainer);
-taskContainer.appendChild(taskTest);
-taskContainer.appendChild(contextTag);
-taskContainer.appendChild(priorityTag);
-taskContainer.appendChild(dueTag);
-taskContainer.appendChild(editOption);
-taskContainer.appendChild(deleteContainer);
-
-// let taskContainerClone = taskContainer.cloneNode(true);
-sectionTask.appendChild(taskContainer.cloneNode(true));
+// taskItem.textContent = getTaskItem(task1);
+// context.textContent = getContext(task1);
+// priority.textContent = getPriority(task1);
+// dueDate.textContent = getDueDate(task1);
 
